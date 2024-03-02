eb64ce7a82a737fb6edde94c25c088a02f015e73
GabrieladcCespedes <160735888+GabrieladcCespedes@users.noreply.github.com>
2024-03-02 02:41:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,39 +10,34 @@ const MAN = document.getElementById('man');
 const DIARIO = document.getElementById('diario');
 
 CALCULAR.addEventListener('click', () => {
-    //console.log('Se hizo click!')
+    console.log('Se hizo click!')
     const PESO = document.getElementById('peso').value
-    //console.log('dato ingresado: ', PESO)
+    console.log('dato ingresado: ', PESO)
     //validamos que se cargue un dato 
     if(PESO > 0){
         ERROR.style.display = 'none';
-        var flujo = calcFlujo(PESO);
-
+        let flujo = calcFlujo(PESO);
+        console.log(calcFlujo(PESO));
         if(PESO<=30){
-            
             let diario = flujo/24;
             let mantenimiento = diario*1.5;
             FLU.innerHTML = "Volumen Diario = " + Math.ceil(flujo) + ' cc/hr ';
             DIARIO.innerHTML = 'Mantenimiento =  ' + Math.ceil(diario) + '  cc';
             MAN.innerHTML = 'Mantenimiento m+m/2 = ' + Math.ceil(mantenimiento) + '  cc/hr';
-            
         } else{
-            
-            let flujoOp1 = flujo * 1500;
-            let flujoOp2 = flujo * 2000;
+            let flujoOp1 = flujo*1500;
             let diarioOp1 = flujoOp1/24;
-            let diarioOp2 = flujoOp2/24;
             let mantenimientoOp1 = diarioOp1*1.5;
-            let mantenimientoOp2 = diario*1.5;
-            FLU.innerHTML = "Volumen Diario = Opción 1: " + flujoOp1 + ' cc/hr ó Opción 2: '+ flujoOp1 + ' cc/hr';
+            let flujoOp2 = flujo*2000;
+            let diarioOp2 = flujoOp2/24;
+            let mantenimientoOp2 = diarioOp2*1.5;
+            FLU.innerHTML = "Volumen Diario = Opción 1: " + Math.ceil(flujoOp1) + ' cc/hr ó Opción 2: '+ Math.ceil(flujoOp2) + ' cc/hr';
             DIARIO.innerHTML = 'Mantenimirnto = Opción 1: ' + Math.ceil(diarioOp1) + ' cc ó Opción 2: ' + Math.ceil(diarioOp2)+' cc';
             MAN.innerHTML = 'Mantenimiornto m+m/2 = Opcion 1: ' + Math.ceil(mantenimientoOp1) + ' cc/hr ó Opción 2: ' + Math.ceil(mantenimientoOp2) +' cc/hr';
-            
         }
         FLU.style.display = 'block';
         MAN.style.display = 'block';
         DIARIO.style.display = 'block';
-
     } else {
         ERROR.style.display = 'block';
         FLU.style.display = 'none';
@@ -52,13 +47,18 @@ CALCULAR.addEventListener('click', () => {
 })
 
 function calcFlujo(peso){
+    let dato;
+    //tuve que implementar (peso*1)+90 porque al solo poner peso+90 me concatenaba y no realizaba la suma correspondiente y separar en variables
     if (peso>30){
-        let superficieCorporal = ((peso*4)/(peso+90));
-        return superficieCorporal;
+        let divisor = (peso*1)+90;
+        let dividendo = (peso*4)+7;
+        dato = dividendo/divisor; 
     } else{
-        return holyday(peso);
+       dato =  holyday(peso);
     }
+    return dato
 }
+
 
 function holyday(peso){
     let flujo;
